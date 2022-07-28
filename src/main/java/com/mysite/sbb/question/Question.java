4314f205a6669615da96bfc082a86b08581397e0
ZeroM2 <kemy21@naver.com>
2022-07-28 13:12:14 +0900

    질문목록과 템플릿사용

---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,34 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.answer.Answer;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answerList;
+}
